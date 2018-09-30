cc56da9ade2ca904ade2bb8ea329d15e157af5fe
Andrew Marcuse <fileformat@gmail.com>
2018-09-30 14:20:29 -0400

    status.json

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,58 @@
 
 import * as Koa from 'koa';
+import * as os from 'os';
 
 const app = new Koa();
 
 
 app.use( async (ctx: Koa.Context) => {
-    ctx.body = 'Hello World!';
+
+    const retVal: {[key: string]: any} = {};
+
+    retVal["success"] = true;
+    retVal["message"] = "OK";
+    retVal["timestamp"] = new Date().toISOString();
+    retVal["lastmod"] = process.env.LASTMOD || null;
+    retVal["commit"] = process.env.COMMIT || null;
+    retVal["__dirname"] = __dirname;
+    retVal["__filename"] = __filename;
+    retVal["os.hostname"] = os.hostname();
+    retVal["os.type"] = os.type();
+    retVal["os.platform"] = os.platform();
+    retVal["os.arch"] = os.arch();
+    retVal["os.release"] = os.release();
+    retVal["os.uptime"] = os.uptime();
+    retVal["os.loadavg"] = os.loadavg();
+    retVal["os.totalmem"] = os.totalmem();
+    retVal["os.freemem"] = os.freemem();
+    retVal["os.cpus.length"] = os.cpus().length;
+    // too much junk: retVal["os.networkInterfaces"] = os.networkInterfaces();
+
+    retVal["process.arch"] = process.arch;
+    retVal["process.cwd"] = process.cwd();
+    retVal["process.execPath"] = process.execPath;
+    retVal["process.memoryUsage"] = process.memoryUsage();
+    retVal["process.platform"] = process.platform;
+    retVal["process.release"] = process.release;
+    retVal["process.title"] = process.title;
+    retVal["process.uptime"] = process.uptime();
+    retVal["process.version"] = process.version;
+    retVal["process.versions"] = process.versions;
+
+    sendJSON(ctx, retVal);
 });
+
+function sendJSON(ctx: Koa.Context, data: object) {
+
+    const callback = ctx.request.query['callback'];
+    if (callback) {
+        ctx.body = callback + '(' + JSON.stringify(data) + ');';
+    } else {
+        ctx.body = JSON.stringify(data);
+    }
+}
+
+app.use
 
 app.listen(4000);
 
