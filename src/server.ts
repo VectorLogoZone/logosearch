7ef268e043772112e9389b70e701d9e04996b132
Andrew Marcuse <fileformat@gmail.com>
2018-09-30 13:59:12 -0400

    koa

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log("TypeScript is working");
+
+import * as Koa from 'koa';
+
+const app = new Koa();
+
+
+app.use( async (ctx: Koa.Context) => {
+    ctx.body = 'Hello World!';
+});
+
+app.listen(4000);
+
+console.log('Server running on port 4000');
